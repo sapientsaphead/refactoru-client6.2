a9ed92316321d1dd5339edc73c7f8bff1323cb30
Debra Zapata <debra.zapata@gmail.com>
2013-10-15 17:38:33 -0600

    In progress.

---
+++ README.md
@@ -0,0 +1,38 @@
+### Status
+
+Click on data opens edit in place. Click on check mark replaces text with new input.
+Does not work more than once.
+
+## Edit in Place
+
+#### Objective
+Create a user profile that can be edited using the in-place editing technique (see [demo](http://designwithpc.com/demo/edit-in-place-jquery-css.html)).
+
+#### Skills
+* Event Handling
+* DOM Manipulation
+* Forms
+
+#### Resources
+* [In Place Editing Demo](http://designwithpc.com/demo/edit-in-place-jquery-css.html)
+
+#### Requirements
+
+#### Part I
+
+1. Create a static user profile with HTML and CSS.
+2. Add a click handler to each text block.
+3. When a text block is clicked, hide the text block and display an input in its place. The    input should take up the same space as the text block so that the transition from text to editable field is seamless and doesn't throw off the layout.
+4. When the input is blurred:
+    1. Hide the input.
+    2. Show the text block.
+    3. Update the text block with the new text.
+
+#### Part II
+
+Refactor your code so that you can apply a class 'editable' to any element within your html and when the page loads your script will automatically make those elements in-place editable.
+
+#### Bonus I
+
+Implement in-place editing with with the X-Editable library. Ignore all the instructions for Ajax and POST requests and follow the note under "Work LOCALLY" to make it work client-side.
+
--- js/main.js
+++ js/main.js
@@ -1,58 +1,6 @@
 $(document).ready(function() {
     
-
-	//HTML RENDERING
-    var insertProfile = function(arr){
-        var profileEl = $('<div class="container"></div>');
-        var nameEl = $('<div class="dataEl"><span class="title">My name is:<br></span><p class="pname">{0}</p></div>'.supplant(arr));
-        var bioEl = $('<div class="dataEl"><span class="title">Short bio:<br></span><p class="pbio">{1}</p></div>'.supplant(arr));
-        var booksEl = $('<div class="dataEl"><span class="title">My favorite books are:<br></span><p class="pfavebooks">{2}</p></div>'.supplant(arr));
-        var libsEl = $('<div class="dataEl"><span class="title">My favorite JS libraries are:<br></span><p class="pfavejslibs">{3}</p></div>'.supplant(arr));
-
-    
-        var inputText = $('<input type="text" class="form-control">');
-        var saveButton = $('<button type"button" class="save btn btn-block btn-primary">✓</button>');
-
-        profileEl.append(nameEl);
-        profileEl.append(bioEl);
-        profileEl.append(booksEl);
-        profileEl.append(libsEl);
-
-        Boolean(myName) === false ? "" : $('.display-profile').append(profileEl);
-        $('.dataEl').append('<div class="inline-editor hide"></div>'); 
-        $('.inline-editor').append(inputText);
-        $('.inline-editor').append(saveButton);  
-   };
-
 	//EVENT HANDLERS
-
-
-    $('.add-profile').on('click', function() {
-    	$('.profile-form').toggle(function (){
-    		$('.profile-form').addClass("active");
-    		$('.profile-form').removeClass("active");
-    	}); 
-    });
-
-    var myName;
-    var myBio;
-    var myFaveBooks;
-    var myFaveJSLibs;
-   
-
-    $('.profile-page').on('click', '.submit', function(){
-
-    	myName = $('#name').val();
-    	myBio = $('#bio').val();
-    	myFaveBooks = $('#fave-books').val();
-    	myFaveJSLibs = $('#fave-jslibs').val();
-
-    	$('.profile-form').toggle(function (){
-    		$('.profile-form').removeClass("active");
-    	}); 
-    	 var profileArray = [myName, myBio, myFaveBooks, myFaveJSLibs];
-    	insertProfile(profileArray);
-    });
 
     $('.display-profile').on('click', 'p', function(){
     	$(this).addClass("hide");
@@ -62,12 +10,12 @@ $(document).ready(function() {
     });
 
     $('.display-profile').on('click', '.save', function(){
-        $(this).closest('.dataEl').find('.inline-editor').removeClass("hide");
-        
-        // console.log(this);
-        //     var newinput = $(input, this).text();
-        //     $('p', this).replaceWith(newinput);
-        });
+        $(this).closest('.dataEl').find('.inline-editor').addClass("hide");
+        var newinput = $(this).closest('.dataEl').find('input').val();
+        $(this).closest('.dataEl').find('p').replaceWith(newinput);
+    });
+
+
    	
 
 }); //end of document ready function
